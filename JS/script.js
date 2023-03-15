06fb2d9a6a4da121cf01e2315fc4558b0dd68d85
bruno-bueno <bbueno1122@gmail.com>
2023-03-15 09:22:52 -0300

    parte2filme

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,49 @@
+let inputBuscarFilme = document.querySelector("#input-buscar-filme");
+let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
 
+
+btnBuscarFilme.onclick = async () => {
+    if(inputBuscarFilme.value.length > 0){
+		let filmes = new Array();
+        fetch("http://www.omdbapi.com/?i=tt3896198&apikey=88ab635&s="+inputBuscarFilme.value)
+		.then((resp)=> resp.json())
+		.then((resp)=> {
+			resp.Search.forEach((item)=>{
+				console.log(item);
+				let filme=new Filme(
+					item.imdbID,
+					item.Title,
+					item.Year,
+					null,
+					null,
+					item.Poster,
+					null,
+					null,
+					null,
+					null,
+					null
+				);
+				filmes.push(filme);
+				
+			});
+			listarFilmes(filmes);
+			
+		})
+		
+    }
+    return false;
+}
+
+
+
+let listarFilmes = async (filmes) => {
+	let listaFilmes = await document.querySelector("#lista-filmes");
+	listaFilmes.innerHTML = "";
+	console.log(listaFilmes);
+	if(filmes.length > 0) {
+		filmes.forEach(async(filme) => {
+			listaFilmes.appendChild(await filme.getCard());
+		});
+	}
+}
     
